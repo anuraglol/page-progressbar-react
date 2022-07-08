e2a9985a0e3600552f2d27881245c04e6ac498ef
Anurag <kr.anurag24@gmail.com>
2022-07-08 17:11:21 +0000

    chore: fix error

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,11 +1,34 @@
-import { Custom } from '../components';
 import { Box } from '@chakra-ui/react';
+import { useTransform, useViewportScroll } from 'framer-motion';
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
 const ProgressPage: NextPage = () => {
+    const { scrollYProgress } = useViewportScroll();
+    const yRange = useTransform(scrollYProgress, [0, 1], [0, 100]);
+
+    const [progress, setProgress] = useState<number>(0);
+
+    useEffect(() => {
+        yRange.onChange(v => {
+            setProgress(v);
+        });
+    }, [yRange]);
+
     return (
         <Box minH="1000vh" w="full" bg="#010101">
-            <Custom />
+            <div
+                style={{
+                    height: '4px',
+                    minWidth: '100vw',
+                    overflowX: 'hidden',
+                    backgroundColor: '#fff',
+                    transformOrigin: 'left',
+                    position: 'fixed',
+                    top: 0,
+                    transform: `scaleX(${progress}%)`,
+                }}
+            ></div>
         </Box>
     );
 };
