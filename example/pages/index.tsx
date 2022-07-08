78acea037c84d5dab1f5fba593916285ecbceb2d
Anurag <kr.anurag24@gmail.com>
2022-07-08 17:09:19 +0000

    chore: chore

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,46 +1,13 @@
-import { Box, CircularProgress } from '@chakra-ui/react';
-import { useTransform, useViewportScroll, motion } from 'framer-motion';
-import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { Custom } from '../components';
+import { Box } from '@chakra-ui/react';
+import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-    const { scrollYProgress } = useViewportScroll();
-    const yRange = useTransform(scrollYProgress, [0, 1], [0, 100]);
-
-    const [progress, setProgress] = useState<number>();
-
-    useEffect(() => {
-        yRange.onChange(v => {
-            setProgress(Math.trunc(v));
-        });
-    }, [yRange]);
-
+const ProgressPage: NextPage = () => {
     return (
-        <Box
-            minH="300vh"
-            w="full"
-            bgGradient="linear(180deg, #f08, #d0e)"
-            overflowX="hidden"
-        >
-            <motion.div>
-                <Box
-                    h="20"
-                    w="20"
-                    bgColor="white"
-                    rounded="16"
-                    pos="fixed"
-                    top="50%"
-                    left="50%"
-                    transform="translateX(-50%) translateY(-50%)"
-                    cursor="pointer"
-                    display="grid"
-                    placeItems="center"
-                >
-                    <CircularProgress as={motion.div} value={progress} />
-                </Box>
-            </motion.div>
+        <Box minH="1000vh" w="full" bg="#010101">
+            <Custom />
         </Box>
     );
 };
 
-export default Home;
+export default ProgressPage;
