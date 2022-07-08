206465b9ccf868640335ca89704efdc24edcb284
Anurag <kr.anurag24@gmail.com>
2022-07-08 09:55:49 +0000

    🎉 feat: changes

--- pages/progress.tsx
+++ pages/progress.tsx
@@ -1,33 +1,11 @@
+import { Custom } from '../components';
 import { Box } from '@chakra-ui/react';
-import { motion, useTransform, useViewportScroll } from 'framer-motion';
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
 
 const ProgressPage: NextPage = () => {
-    const { scrollYProgress } = useViewportScroll();
-    const yRange = useTransform(scrollYProgress, [0, 1], [0, 100]);
-
-    const [progress, setProgress] = useState<number>(0);
-
-    useEffect(() => {
-        yRange.onChange(v => {
-            setProgress(v);
-        });
-    }, [yRange]);
-
     return (
         <Box minH="1000vh" w="full" bg="#010101">
-            <motion.div>
-                <Box
-                    pos="fixed"
-                    top="0"
-                    h="1"
-                    w="full"
-                    bg="pink.600"
-                    transform={`scaleX(${progress}%)`}
-                    transformOrigin="left"
-                ></Box>
-            </motion.div>
+            <Custom />
         </Box>
     );
 };
