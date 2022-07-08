97a11905971e0b697d36a3b5b47de684219e3a03
Anurag <kr.anurag24@gmail.com>
2022-07-08 21:22:06 +0000

    feat: successfully publish to npm

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,34 +1,11 @@
 import { Box } from '@chakra-ui/react';
-import { useTransform, useViewportScroll } from 'framer-motion';
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import PageProgress from 'page-scroll-progressbar';
 
 const ProgressPage: NextPage = () => {
-    const { scrollYProgress } = useViewportScroll();
-    const yRange = useTransform(scrollYProgress, [0, 1], [0, 100]);
-
-    const [progress, setProgress] = useState<number>(0);
-
-    useEffect(() => {
-        yRange.onChange(v => {
-            setProgress(v);
-        });
-    }, [yRange]);
-
     return (
         <Box minH="1000vh" w="full" bg="#010101">
-            <div
-                style={{
-                    height: '4px',
-                    minWidth: '100vw',
-                    overflowX: 'hidden',
-                    backgroundColor: '#ec4899',
-                    transformOrigin: 'left',
-                    position: 'fixed',
-                    top: 0,
-                    transform: `scaleX(${progress}%)`,
-                }}
-            ></div>
+            <PageProgress />
         </Box>
     );
 };
--- package/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as PageProgress } from "./PageProgress.misc";
